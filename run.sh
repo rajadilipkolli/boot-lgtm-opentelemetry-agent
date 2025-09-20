9cfb433dd73cd8d44d6d40defc59955ce6d52525
Raja Kolli <rajadilipkolli@gmail.com>
2025-09-20 12:50:02 +0000

    fix : enable prometheus actuator

--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ else
 fi
 
 export OTEL_RESOURCE_ATTRIBUTES="service.name=coupon,service.instance.id=localhost:8080"
+export OTEL_EXPORTER_OTLP_METRICS_DEFAULT_HISTOGRAM_AGGREGATION="explicit_bucket_histogram"
 export OTEL_LOGS_EXPORTER="otlp"
 
 java -Dotel.metric.export.interval=500 -Dotel.bsp.schedule.delay=500 -javaagent:"./build/agent/opentelemetry-javaagent.jar" -jar ./build/libs/app.jar
